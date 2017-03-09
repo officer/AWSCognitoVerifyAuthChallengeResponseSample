eab7534e5f2b551e4bf1ca0471b7f579965adb88
chocolatle <notbeafraidofanything@gmail.com>
2017-03-10 02:06:17 +0900

    Add project files.

---
+++ VerifyAuthChallengeResponse/app.js
@@ -0,0 +1,22 @@
+﻿console.log('Loading');
+
+exports.handler = function (event, context) {
+
+    if (event != null) {
+        console.log('event = ' + JSON.stringify(event));
+    }
+    else {
+        console.log('No event object');
+
+    }
+
+    if (event.request.privateChallengeParameters.answer == event.request.challengeAnswer) {
+        event.response.answerCorrect = true;
+    } else {
+        event.response.answerCorrect = false;
+    }
+
+    console.log('event = ' + JSON.stringify(event));
+
+    context.done(null, event);  // SUCCESS with message
+};
